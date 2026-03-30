6ea1b64d9b74152c1432b44035d814f430392f17
Park, Daekyeong <pdk2037@gmail.com>
2026-03-30 21:56:54 +0900

    feat/#2: 유저 관계를 저장할 UserRelation 테이블 추가

---
+++ src/main/java/kr/omong/todagtodag/domain/relation/entity/UserRelation.java
@@ -0,0 +1,48 @@
+package kr.omong.todagtodag.domain.relation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import kr.omong.todagtodag.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@Table(
+        name = "user_relation",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"todak_id", "sungjang_id"})
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserRelation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todak_id", nullable = false)
+    private User todak;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sungjang_id", nullable = false)
+    private User sungjang;
+
+    public static UserRelation of(User todak, User sungjang) {
+        return UserRelation.builder()
+                .todak(todak)
+                .sungjang(sungjang)
+                .build();
+    }
+}

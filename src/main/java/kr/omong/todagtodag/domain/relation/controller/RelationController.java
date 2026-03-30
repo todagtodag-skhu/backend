f889b06b86c9f6cbf07c171bebac7d822ad036aa
Park, Daekyeong <pdk2037@gmail.com>
2026-03-30 22:44:52 +0900

    docs/#2: swagger 문서에 적용될 설명 추가

--- src/main/java/kr/omong/todagtodag/domain/relation/controller/RelationController.java
+++ src/main/java/kr/omong/todagtodag/domain/relation/controller/RelationController.java
@@ -1,5 +1,9 @@
 package kr.omong.todagtodag.domain.relation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.omong.todagtodag.domain.relation.dto.UserRelationConnectRequest;
 import kr.omong.todagtodag.domain.relation.dto.UserRelationInviteCodeResponse;
 import kr.omong.todagtodag.domain.relation.service.RelationService;
@@ -14,10 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/relation")
 @RequiredArgsConstructor
+@Tag(name = "유저 관계 설정 API", description = "유저의 관계를 설정하기 위해 초대 코드 발급 및 인증 수행")
 public class RelationController {
 
     private final RelationService relationService;
 
+    @Operation(
+            summary = "성장이 - 코드 생성",
+            description =
+                    """
+                    성장이가 코드를 생성합니다.
+                    
+                    경로 변수에 성장이 유저 id가 필요하며, 생성된 코드를 body로 반환합니다.
+                    
+                    생성된 코드는 5분 동안 유지됩니다.
+                    
+                    토닥이 유저가 이 API를 실행할 경우, 에러가 발생합니다.
+                    """
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "코드 생성 성공"),
+            @ApiResponse(responseCode = "403", description = "토큰이 없거나, 성장이 유저로 요청"),
+            @ApiResponse(responseCode = "404", description = "유저가 존재하지 않음")
+    })
     @PostMapping("/invite-code/{sungjangId}")
     public ResponseEntity<UserRelationInviteCodeResponse> generateInviteCode(
             @PathVariable Long sungjangId
@@ -26,6 +49,24 @@ public class RelationController {
         return ResponseEntity.ok(new UserRelationInviteCodeResponse(code));
     }
 
+    @Operation(
+            summary = "토닥이 - 코드 입력",
+            description =
+                    """
+                    토닥이 유저가 코드를 입력하여 성장이와 연결합니다.
+                    
+                    경로 변수에 토닥이 유저 id가 필요하고, code를 body로 입력해야 합니다.
+                    
+                    성장이 유저가 이 API를 실행할 경우, 그리고 이미 관계가 존재할 경우 에러가 발생합니다.
+                    """
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "코드 입력 성공, 관계 연결"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 코드"),
+            @ApiResponse(responseCode = "403", description = "토큰이 없거나, 토닥이 유저로 요청"),
+            @ApiResponse(responseCode = "404", description = "유저가 존재하지 않음"),
+            @ApiResponse(responseCode = "409", description = "이미 존재하는 유저 관계")
+    })
     @PostMapping("/connect/{todakId}")
     public ResponseEntity<Void> connect(
             @PathVariable Long todakId,
